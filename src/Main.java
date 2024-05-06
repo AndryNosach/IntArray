62eb5158ff88e8acc0c55d98e0139b76a5270579
AndryNosach <andry.nosach@gmail.com>
2024-05-06 13:10:11 +0300

    added elements to array

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        IntArray array = new IntArray(new int[]{23, 223, 10, -50, 125, 222, 50, 34, 1, -56, -100});
+        IntArray array = new IntArray(new int[]{23, 223, 10, -50, 125, 222, 50, 34, 1, -56, -100, -33, 1024, 11, 10, 88, 19, 454, 10});
 
         for (int i =0; i < array.size(); i++) {
             System.out.println(array.get(i));
